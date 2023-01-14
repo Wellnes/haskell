19bf68c188bc42a6600f7a739563c101e639adfb
Мирза Дмитрий <spec1cod@yandex.ru>
2023-01-14 15:53:50 +0300

    Add files via upload

--- lesson5.hs
+++ lesson5.hs
@@ -7,3 +7,21 @@ ifEven f x = if even x
              else x
 
 genIfEvenX x = (\f -> ifEven f x)
+
+getRequestUrl host apiKey resourse id = 
+  host ++ "/" ++ resourse ++ "/" ++ id ++ "?token=" ++ apiKey
+
+genHostRequestBuilder host = (\apiKey resourse id -> 
+                               getRequestUrl host apiKey resourse id)
+
+exampleUrlBuilder = genHostRequestBuilder "http://example.com"
+
+genApiRequestBuilder hostBuilder apiKey = 
+              (\resourse id -> hostBuilder apiKey resourse id)
+
+myExampleUrlBuilder = genApiRequestBuilder exampleUrlBuilder "1337hAsk3ll"
+
+genResourseApiRequestBuilder hostBuilder apiKey resourse =
+     (\id -> hostBuilder apiKey resourse id)
+
+rExampleUrlBuilder = genResourseApiRequestBuilder exampleUrlBuilder "1337hAsk3ll" "book"
