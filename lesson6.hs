62d358a225b1a0ad39c4ce46bd9ff07a3912e4b7
Мирза Дмитрий <spec1cod@yandex.ru>
2023-01-18 16:50:35 +0300

    Add files via upload

--- lesson6.hs
+++ lesson6.hs
@@ -3,3 +3,10 @@ isPalindrome word = word == reverse word
 respond phrase = if '!' `elem` phrase
                  then "Ogo!"
                  else "Ny, ok"
+
+takeLast n aList = reverse( take n (reverse aList) )
+
+ones n = take n (cycle [1])
+
+assingToGroups n aList = zip groups aList
+    where groups = cycle [ 1..n ]
