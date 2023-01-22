908a76b49df3141d3f4fde745114d4ecb1365f54
Мирза Дмитрий <spec1cod@yandex.ru>
2023-01-22 15:04:56 +0300

    Add files via upload

--- lesson6.hs
+++ lesson6.hs
@@ -10,3 +10,20 @@ ones n = take n (cycle [1])
 
 assingToGroups n aList = zip groups aList
     where groups = cycle [ 1..n ]
+
+-- HW 6.1
+
+myRepeat n = take 1 aList
+  where aList = cycle [n]
+
+-- HW 6.2
+
+subseq start stop vxList = take diff dList
+  where diff = stop - start
+        dList = drop start vxList
+
+dlinaSp vxList = length vxList
+
+inFirstHalf val vxList = elem val firstHalf
+   where midpoint = (length vxList) `div` 2
+         firstHalf = take midpoint vxList
