d8abb2a29940df0386c253050e4cc0bb2483e359
Мирза Дмитрий <spec1cod@yandex.ru>
2023-01-26 16:40:55 +0300

    Add files via upload

--- lesson8.hs
+++ lesson8.hs
@@ -1,3 +1,8 @@
 myLength [] = 0
 --myLength xs = 1 + myLength( tail xs )
 myLength (_:xs) = 1 + myLength xs 
+
+myTake _ [] = []
+myTake 0 _ = []
+myTake n (x:xs) = x:rest
+  where rest = myTake (n - 1) xs
