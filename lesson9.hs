0621a46242b8480a606d67069e33166f9fcd54b8
Мирза Дмитрий <spec1cod@yandex.ru>
2023-01-31 16:11:51 +0300

    Add files via upload

--- lesson9.hs
+++ lesson9.hs
@@ -6,3 +6,13 @@ squareAll (x:xs) = x^2 : squareAll xs
 
 myMap f [] = []
 myMap f (x:xs) = (f x) : myMap f xs
+
+myFilter test [] = []
+myFilter test (x:xs) = if test x
+                       then x:myFilter test xs
+                       else myFilter test xs
+
+myRemove test [] = []
+myRemove test (x:xs) = if test x
+                       then myRemove test xs
+                       else x:myRemove test xs 
