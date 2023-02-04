60d3b8b96a4a3ed014a941b1109b6fc61c7939b6
Мирза Дмитрий <spec1cod@yandex.ru>
2023-02-04 11:32:32 +0300

    Add files via upload

--- lesson9.hs
+++ lesson9.hs
@@ -25,3 +25,11 @@ sumOfSquares xs = foldl (+) 0 (map (^2) xs)
 
 rcons xs y = y:xs
 myReverse xs = foldl rcons [] xs
+
+myFoldl f init [] = init
+myFoldl f init (x:xs) = myFoldl f newInit xs
+  where newInit = f init x
+
+myFoldr f init [] = init
+myFoldr f init (x:xs) = f x rightResult
+  where rightResult = myFoldr f init xs
