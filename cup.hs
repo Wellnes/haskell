091a2d209b9d91f615f712b43e2c5e7c9e8821a3
Мирза Дмитрий <spec1cod@yandex.ru>
2023-02-07 17:06:28 +0300

    Add files via upload

---
+++ cup.hs
@@ -0,0 +1,11 @@
+cup ml = \message -> message ml
+
+getMl aCup = aCup (\ml -> ml)
+
+drink aCup mlDrank = if mlDiff >= 0
+                     then cup mlDiff
+                     else cup 0
+  where ml = getMl aCup
+        mlDiff = ml - mlDrank
+
+isEmpty aCup = getMl aCup == 0
